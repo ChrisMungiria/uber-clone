6d66483c3a2eb26f2106dc209308addaae9a08b7
Chris Mungiria <christopher.mungiria@strathmore.edu>
2024-03-13 16:11:28 +0300

    feat: built the NavgationCard component

---
+++ components/NavigateCard.js
@@ -0,0 +1,60 @@
+import { SafeAreaView, Text, View } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
+
+const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView className="bg-white flex-1">
+      <Text className="text-center py-5 text-xl">Good Morning, User</Text>
+      <View className="border-t border-gray-200 flex-shrink">
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder="Where to?"
+            debounce={400}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            styles={{
+              container: {
+                backgroundColor: "white",
+                paddingTop: 20,
+                flex: 0,
+              },
+              textInput: {
+                backgroundColor: "#DDDDDF",
+                borderRadius: 0,
+                fontSize: 18,
+              },
+              textInputContainer: {
+                paddingHorizontal: 20,
+                paddingBottom: 0,
+              },
+            }}
+            returnKeyType={"search"}
+            minLength={2}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            query={{
+              key: GOOGLE_MAPS_API_KEY,
+              language: "en",
+            }}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionsCard");
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default NavigateCard;
